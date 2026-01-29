cadd2253d70d2c0afb48130a9478355e18d32a01
asonkes <audrey.sonkes@gmail.com>
2026-01-29 22:31:46 +0100

    css exercice préparation back

--- demo-react/src/exos/5_Exo_Formulaire/Exo5Component.jsx
+++ demo-react/src/exos/5_Exo_Formulaire/Exo5Component.jsx
@@ -8,19 +8,30 @@ export const Exo5Component = () => {
         <div className={style.exo5}>
             <h2>Notre calculatrice :</h2>
             <div className={style.exo5Container}>
-                <p>
-                    <span>Nb1 : </span>
-                    <span></span>
-                </p>
-                <p>
-                    <span>Nb2 :</span>
-                    <span></span>
-                </p>
+                <div className={style.exo5Numbers}>
+                    <p>
+                        <span className={style.text1}>Nb1 : </span>
+                        <span className={style.text2}>0</span>
+                    </p>
+                    <div className={style.exo5Operator}>
+                        <p className={style.text1}>Opération : </p>
+                        <select name="" id="">+
+                            <option>+</option>
+                            <option>-</option>
+                            <option>x</option>
+                            <option>/</option>
+                        </select>
+                    </div>
+                    <p>
+                        <span className={style.text1}>Nb2 :</span>
+                        <span className={style.text2}>0</span>
+                    </p>
+                </div>
+                <div className={style.exo5Calculator}>
+                    <button>Calculer</button>
+                    <p className={style.text2}>...</p>
+                </div>
             </div>
-
-
-
-
         </div>
     )
 }
